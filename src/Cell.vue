74839814d0ee065cc5279d8dc609271de7ba495a
Feng <13823532026@163.com>
2019-09-20 01:15:24 +0800

    vue实现井字小游戏

---
+++ src/Cell.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="cell" v-on:click="onClickSelf">
+      <template v-if="a">{{text}}</template>
+      <template v-else></template>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["n"],
+  data() {
+    return { a: false, text: "" };
+  },
+
+  methods: {
+    onClickSelf() {
+      if (this.text != "") {
+        return;
+      }
+      this.a = true;
+      this.text = this.n % 2 === 0 ? "x" : "o";
+      this.$emit("click", this.text);
+    }
+  }
+};
+</script>
+
+<style>
+.cell {
+  border: solid 1px black;
+  width: 100px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 50px;
+}
+</style>
